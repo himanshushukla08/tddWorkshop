ca18fdabb2952223c7d582166d0191192e4a99a7
Gregory Haskins <greg@greghaskins.com>
2017-06-16 11:17:15 -0600

    Post-demo

--- renewalMockKata/src/main/java/RenewalNotificationService.java
+++ renewalMockKata/src/main/java/RenewalNotificationService.java
@@ -1,18 +1,29 @@
-import java.util.Arrays;
+import java.time.LocalDate;
+import java.util.List;
 
 public class RenewalNotificationService {
 
   private SubscriberService subscriberService;
   private EmailService emailService;
+  private LocalDate today;
 
-  public RenewalNotificationService(SubscriberService subscriberService, EmailService emailService) {
+  public RenewalNotificationService(SubscriberService subscriberService, EmailService emailService,
+      LocalDate today) {
     this.subscriberService = subscriberService;
     this.emailService = emailService;
+    this.today = today;
   }
 
   public void notifyAtRiskSubscribers() {
+    LocalDate threeMonthsFromToday = today.plusMonths(3);
+
+    List<String> subscribers = subscriberService.getSubscribersThatWillExpireBetweenNowAndDate(
+        threeMonthsFromToday.getDayOfMonth(), 
+        threeMonthsFromToday.getMonthValue(),
+        threeMonthsFromToday.getYear());
+
     emailService.emailMessage(
         "Please renew your subscription to Ferret Fancy!",
-        Arrays.asList("mrcoolguy@example.com"));
+        subscribers);
   }
 }
--- renewalMockKata/src/test/java/RenewalNotificationServiceTest.java
+++ renewalMockKata/src/test/java/RenewalNotificationServiceTest.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,18 +31,99 @@ public class RenewalNotificationServiceTest {
 
   }
 
+  public class MockSubscriberService implements SubscriberService {
+
+    public List<String> subscribers = new ArrayList<>();
+    public int day;
+    public int month;
+    public int year;
+
+    @Override
+    public List<String> getSubscribersThatWillExpireBetweenNowAndDate(int day, int month, int year) {
+      this.day = day;
+      this.month = month;
+      this.year = year;
+      return this.subscribers ;
+    }
+
+  }
+
   @Test
   public void notifyingAtRiskSubscribersShouldSendEmails() {
     // Arrange
     MockEmailService emailService = new MockEmailService();
+    MockSubscriberService subscriberService = new MockSubscriberService();
+    subscriberService.subscribers.add("mrcoolguy@example.com");
+    
     RenewalNotificationService renewalNotificationService =
-        new RenewalNotificationService(new RealSubscriberService(), emailService);
+        new RenewalNotificationService(subscriberService, emailService, LocalDate.now());
 
     // Act
     renewalNotificationService.notifyAtRiskSubscribers();
-    
+
     // Assert
     assertEquals("Please renew your subscription to Ferret Fancy!", emailService.getLastMessageSent());
     assertEquals(Arrays.asList("mrcoolguy@example.com"), emailService.getLastEmailAddresses());
   }
+
+  @Test
+  public void shouldGetListOfEmailsFromTheSubscriberService() {
+    // Arrange
+    MockEmailService emailService = new MockEmailService();
+    MockSubscriberService subscriberService = new MockSubscriberService();
+    subscriberService.subscribers.add("mrcoolguy@example.com");
+    subscriberService.subscribers.add("ferretFan121@hotmail.co.org.uk.io");
+
+    RenewalNotificationService renewalNotificationService =
+        new RenewalNotificationService(subscriberService, emailService, LocalDate.now());
+
+    // Act
+    renewalNotificationService.notifyAtRiskSubscribers();
+
+    // Assert
+    assertEquals("Please renew your subscription to Ferret Fancy!", emailService.getLastMessageSent());
+    assertEquals(Arrays.asList("mrcoolguy@example.com", "ferretFan121@hotmail.co.org.uk.io"),
+        emailService.getLastEmailAddresses());
+  }
+  
+  @Test
+  public void shouldQueryTheSubscriberServiceWithADateThatIsThreeMonthsAway_Example1() {
+    LocalDate today = LocalDate.of(1776, 4, 4);
+    
+    // Arrange
+    MockEmailService emailService = new MockEmailService();
+    MockSubscriberService subscriberService = new MockSubscriberService();
+
+    RenewalNotificationService renewalNotificationService =
+        new RenewalNotificationService(subscriberService, emailService, today);
+
+    // Act
+    renewalNotificationService.notifyAtRiskSubscribers();
+
+    // Assert
+    assertEquals(4, subscriberService.day);
+    assertEquals(7, subscriberService.month);
+    assertEquals(1776, subscriberService.year);
+  }
+  
+  @Test
+  public void shouldQueryTheSubscriberServiceWithADateThatIsThreeMonthsAway_Example2() {
+    // Arrange
+    MockEmailService emailService = new MockEmailService();
+    MockSubscriberService subscriberService = new MockSubscriberService();
+    LocalDate today = LocalDate.of(1955, 8, 5);
+
+    RenewalNotificationService renewalNotificationService =
+        new RenewalNotificationService(subscriberService, emailService, today);
+
+    // Act
+    renewalNotificationService.notifyAtRiskSubscribers();
+
+    // Assert
+    assertEquals(5, subscriberService.day);
+    assertEquals(11, subscriberService.month);
+    assertEquals(1955, subscriberService.year);
+  }
+  
+  
 }

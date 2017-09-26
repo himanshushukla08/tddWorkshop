bfd3d4a6255d84af89528a97af655764915bdfb8
davidkessler-wk <david.kessler@workiva.com>
2017-09-26 15:55:46 -0500

    RomanNumerals: added test case for II + II = IV

---
+++ romanNumerals/src/main/java/sample/SampleRomanNumeralCalculator.java
@@ -0,0 +1,35 @@
+package sample;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SampleRomanNumeralCalculator {
+
+    private static Map<String, Integer> translation;
+
+    static {
+        translation = Collections.unmodifiableMap(new HashMap<String, Integer>() {
+            {
+                put("I", 1);
+            }
+        });
+    }
+
+    public String add(String numeralOne, String numeralTwo) {
+        int oneCount = count(numeralOne, numeralTwo, "I");
+        if(oneCount == 4) return "IV";
+        return numeralOne + numeralTwo;
+    }
+
+    private int count(String numeralOne, String numeralTwo, String target) {
+        int total = 0;
+        for (char character : (numeralOne + numeralTwo).toCharArray()) {
+            String letter = String.valueOf(character);
+            if(letter.equals(target)){
+                total++;
+            }
+        }
+        return total;
+    }
+}
---
+++ romanNumerals/src/test/java/sample/SampleRomanNumeralCalculatorTest.java
@@ -0,0 +1,36 @@
+package sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SampleRomanNumeralCalculatorTest {
+
+  @Test
+  public void onePlusOneIsTwo() {
+    SampleRomanNumeralCalculator calculator = new SampleRomanNumeralCalculator();
+
+    assertEquals("II", calculator.add("I", "I"));
+  }
+
+  @Test
+  public void onePlusTwoIsThree() {
+    SampleRomanNumeralCalculator calculator = new SampleRomanNumeralCalculator();
+
+    assertEquals("III", calculator.add("I", "II"));
+  }
+
+  @Test
+  public void TwoPlusOneIsThree() {
+    SampleRomanNumeralCalculator calculator = new SampleRomanNumeralCalculator();
+
+    assertEquals("III", calculator.add("II", "I"));
+  }
+
+  @Test
+  public void TwoPlusTwoIsFour() {
+    SampleRomanNumeralCalculator calculator = new SampleRomanNumeralCalculator();
+
+    assertEquals("IV", calculator.add("II", "II"));
+  }
+}

6ab4d9b4970be7aef7e67226eb8f209166ff23a9
davidkessler-wk <david.kessler@workiva.com>
2017-09-26 11:49:29 -0500

    Refactor: extracted conditions into methods for clarity

--- tennisScoring/src/main/java/sample/SampleTennisGameMedium.java
+++ tennisScoring/src/main/java/sample/SampleTennisGameMedium.java
@@ -20,28 +20,13 @@ public class SampleTennisGameMedium implements ISampleTennisGame {
     public String getScore() {
         String score = "";
         int tempScore = 0;
-        if (playerOne.getPoints() == playerTwo.getPoints()) {
-            switch (playerOne.getPoints()) {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        } else if (playerOne.getPoints() >= 4 || playerTwo.getPoints() >= 4) {
-            int minusResult = playerOne.getPoints() - playerTwo.getPoints();
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+        if (isTied(playerOne, playerTwo)) {
+            score = calculateTiedScore();
+        } else if (atLeastOnePlayerHasFourPoints()) {
+            if (playerOneAdvantage()) score = "Advantage player1";
+            if (playerTwoAdvantage()) score = "Advantage player2";
+            if (playerOneWinner()) score = "Win for player1";
+            if (playerTwoWinner()) score = "Win for player2";
         } else {
             for (int i = 1; i < 3; i++) {
                 if (i == 1) tempScore = playerOne.getPoints();
@@ -49,22 +34,75 @@ public class SampleTennisGameMedium implements ISampleTennisGame {
                     score += "-";
                     tempScore = playerTwo.getPoints();
                 }
-                switch (tempScore) {
-                    case 0:
-                        score += "Love";
-                        break;
-                    case 1:
-                        score += "Fifteen";
-                        break;
-                    case 2:
-                        score += "Thirty";
-                        break;
-                    case 3:
-                        score += "Forty";
-                        break;
-                }
+                score = calculateScore(score, tempScore);
             }
         }
         return score;
+    }
+
+    private boolean playerOneWinner() {
+        return minusResult() >= 2;
+    }
+
+    private boolean playerTwoWinner() {
+        return minusResult() <= -2;
+    }
+
+    private boolean playerTwoAdvantage() {
+        return minusResult() == -1;
+    }
+
+    private boolean playerOneAdvantage() {
+        return minusResult() == 1;
+    }
+
+    private int minusResult() {
+        return playerOne.getPoints() - playerTwo.getPoints();
+    }
+
+    private boolean atLeastOnePlayerHasFourPoints() {
+        return playerOne.getPoints() >= 4 || playerTwo.getPoints() >= 4;
+    }
+
+    private String calculateScore(String score, int tempScore) {
+        switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
+        }
+        return score;
+    }
+
+    private String calculateTiedScore() {
+        String score;
+        switch (playerOne.getPoints()) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        return score;
+    }
+
+    private boolean isTied(Player playerOne, Player playerTwo) {
+        return playerOne.getPoints() == playerTwo.getPoints();
     }
 }

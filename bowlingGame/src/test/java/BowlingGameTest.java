edf450c581f22d9b7b3bc54ca7137d2bc7f64259
davidkessler-wk <david.kessler@workiva.com>
2017-09-28 09:33:59 -0500

    added passing tests for games scoring 2 and 20

--- bowlingGame/src/test/java/BowlingGameTest.java
+++ bowlingGame/src/test/java/BowlingGameTest.java
@@ -13,4 +13,14 @@ public class BowlingGameTest {
     public void scoreOneWhenTheFirstRollThrowOnePinAndMissOtherRolls() {
         assertEquals(1, new BowlingGame().score("1-------------------"));
     }
+
+    @Test
+    public void scoreTwoWhenTheFirstRollThrowOnePinAndMissOtherRolls() {
+        assertEquals(2, new BowlingGame().score("2-------------------"));
+    }
+
+    @Test
+    public void scoreTwentyWhenAllRollsThrowOnePin() {
+        assertEquals(20, new BowlingGame().score("11111111111111111111"));
+    }
 }

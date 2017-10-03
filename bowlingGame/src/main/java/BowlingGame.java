92ceb7cec78939ce4bad6d5e4c44e44cbe2f4b23
davidkessler-wk <david.kessler@workiva.com>
2017-10-03 07:15:36 -0500

    added case for strike followed by two bonus rolls

--- bowlingGame/src/main/java/BowlingGame.java
+++ bowlingGame/src/main/java/BowlingGame.java
@@ -1,30 +1,51 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BowlingGame {
 
     public int score(String rolls) {
         int score = 0;
-        int previousScore = 0;
         boolean hasSpare = false;
+        int scoreOneBack = 0;
+        int scoreTwoBack = 0;
 
-        for (Character character : rolls.toCharArray()) {
+        for (String stringScore : reverse(rolls)) {
             try {
-                int currentScore = Integer.valueOf(character.toString());
+                int currentScore = Integer.valueOf(stringScore);
 
-                if (hasSpare) {
-                    score += previousScore;
+                if (!hasSpare) {
+                    score += currentScore;
+                } else {
+                    hasSpare = false;
                 }
 
-                score += currentScore;
-                previousScore = currentScore;
+                scoreTwoBack = scoreOneBack;
+                scoreOneBack = currentScore;
             } catch (NumberFormatException exception) {
-                if ("X".equals(character.toString())) {
+                if ("X".equals(stringScore)) {
+                    score += scoreOneBack;
+                    score += scoreTwoBack;
                     score += 10;
                 }
-                if ("/".equals(character.toString())) {
-                    score += (10 - previousScore);
+                if ("/".equals(stringScore)) {
+                    score += 10;
+                    score += scoreOneBack;
                     hasSpare = true;
                 }
             }
         }
         return score;
+    }
+
+    private List<String> reverse(String rolls) {
+        ArrayList<String> scores = new ArrayList<>();
+
+        for (Character character : rolls.toCharArray()) {
+            scores.add(Character.toString(character));
+        }
+
+        Collections.reverse(scores);
+        return scores;
     }
 }

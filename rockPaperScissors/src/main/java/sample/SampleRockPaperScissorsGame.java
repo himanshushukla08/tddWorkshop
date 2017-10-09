1c6e1ea4aa2f95a9ac55a9f165d19bb4050d30d2
davidkessler-wk <david.kessler@workiva.com>
2017-10-08 21:46:59 -0500

    refactor: extracted constants to remove duplication; extracted duplicate message creation into private method,

--- rockPaperScissors/src/main/java/sample/SampleRockPaperScissorsGame.java
+++ rockPaperScissors/src/main/java/sample/SampleRockPaperScissorsGame.java
@@ -2,11 +2,19 @@ package sample;
 
 public class SampleRockPaperScissorsGame {
 
-    public String determineWinner(String gesture1, String gesture2) {
-        if ("paper".equals(gesture1) && "rock".equals(gesture2)) {
-            return "paper beats rock";
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+
+    public String determineWinner(String gestureOne, String gestureTwo) {
+        if (PAPER.equals(gestureOne) && ROCK.equals(gestureTwo)) {
+            return createMessage(gestureOne, gestureTwo);
         }
-        return "rock beats scissors";
+        return createMessage(ROCK, SCISSORS);
+    }
+
+    private String createMessage(String gestureOne, String gestureTwo) {
+        return gestureOne + " beats " + gestureTwo;
     }
 
 }

638e54ef291b608af8b0a121c835c5d78f827005
davidkessler-wk <david.kessler@workiva.com>
2017-10-10 07:16:51 -0500

    refactor: cut over to enhanced for loop to remove index lookup duplication

--- gildedRose/src/main/java/sample/SampleGildedRose.java
+++ gildedRose/src/main/java/sample/SampleGildedRose.java
@@ -20,7 +20,7 @@ public class SampleGildedRose {
 
 
     public List<SampleItem> updateQuality() {
-        for (int i = 0; i < inventory.size(); i++) {
+        for (SampleItem item : inventory) {
 //            if ((!"Aged Brie".equals(inventory.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(inventory.get(i).getName())) {
 //                if (inventory.get(i).getQuality() > 0) {
 //                    if (!"Sulfuras, Hand of Ragnaros".equals(inventory.get(i).getName())) {
@@ -47,8 +47,8 @@ public class SampleGildedRose {
 //                }
 //            }
 //
-            if (!"Sulfuras, Hand of Ragnaros".equals(inventory.get(i).getName())) {
-                inventory.get(i).setSellIn(inventory.get(i).getSellIn() - 1);
+            if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+                item.setSellIn(item.getSellIn() - 1);
             }
 //
 //            if (inventory.get(i).getSellIn() < 0) {

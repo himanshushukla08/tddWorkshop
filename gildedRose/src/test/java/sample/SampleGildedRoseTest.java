e9a80755b2673c4ccb05db3a5d5fba9fc37dbf06
davidkessler-wk <david.kessler@workiva.com>
2017-10-10 06:51:33 -0500

    created sample package and classes

---
+++ gildedRose/src/test/java/sample/SampleGildedRoseTest.java
@@ -0,0 +1,14 @@
+package sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class SampleGildedRoseTest {
+
+    @Test
+    public void testTheTruth() {
+        assertTrue(true);
+    }
+}
